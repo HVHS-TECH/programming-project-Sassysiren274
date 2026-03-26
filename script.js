32c0f9691bed2d8b2f90af5571bc045ac12430d2
Sassysiren274 <25471jf@hvhs.school.nz>
2026-03-27 09:59:21 +1300

    ,m kjhnjm

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ let lastFloorX = 0;
 let vel = 3
 let coins = [];
 let score = 0;
+let gameOver = false;
 //
 function setup() {
   console.log("setup");
   new Canvas(1250, 650);
   world.gravity.y = 10;
-//
+  //
   player = new Sprite(60, 0, 30, 30);
   player.color = 'blue';
-//
+  //
   camera.on();
   //
   for (let i = 0; i < 10; i++) {
@@ -39,7 +40,7 @@ function setup() {
 //draw()
 // *****************************************************/
 function draw() {
-  background('#45caf3;');
+  background('#6cc7eb');
   //clear();
 
   // movement
@@ -65,13 +66,47 @@ function draw() {
   camera.x = player.x;
 
   // generating more floor when player moves forward
+ // console.log(player. 1);
+      score += 1;
+    }
+  }
+  //
+  if (random() > 0.4) {
+    let coin = new Sprite(floor.x,- 60, 10, 10); //random(100, 400), random(0, 150), 10, 10);
+    coin.color = 'yellow';
+    coin.type = 'coin';x)
+ // console.log(lastFloorX - 300)
   if (player.x > lastFloorX - 300) {
+   // console.log("Make new floor")
     generateFloor();
   }
   //
+  for (let i = coins.length - 1; i >= 0; i--) {
+    if (player.overlaps(coins[i])) {
+      coins[i].remove();
+      coins.splice(i,
+    coins.push(coin);
+  }
+  console.log(coins.length)
+  // 
   textSize(20);
   fill(0);
   text("Score: " + score, 10, 20);
+  //
+  if (player.y > height + 10) {
+  gameOver = true;
+}
+  //
+  if (gameOver) {
+  textSize(50);
+  fill('red');
+  text("GAME OVER", camera.x - 150, height / 2);
+  return; 
+}
+//
+if (gameOver && kb.presses('r')) {
+  location.reload(); 
+}
 }
 
 function generateFloor() {
@@ -81,11 +116,10 @@ function generateFloor() {
     // random gaps
     //  if (random() > 0.3)
 
-       let floor = new Sprite(lastFloorX + 250, 200, 500, 40, 'static');
-       floor.color = 'green';
-       floors.push(floor);
-    lastFloorX+= 500 + random(300,450)
-
+    let floor = new Sprite(lastFloorX + 250, 200, 500, 40, 'static');
+    floor.color = 'green';
+    floors.push(floor);
+    lastFloorX += 500 + random(300, 450)
     // lastFloorX += 100;
   }
 }
